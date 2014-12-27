19abf56edc594a5e6c7ac063ab85b87500f6dacc
scott.lan <scott.lan@taisys.com>
2014-12-27 11:59:15 +0800

    add remote server sample

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 # coding: utf-8
 
 from py4j.java_gateway import JavaGateway
+from py4j.java_gateway import GatewayClient
 
 if __name__ == '__main__':
-    gateway = JavaGateway()
+	# gateway = JavaGateway()
+	# you can use remote server
+    gateway = JavaGateway(
+    	gateway_client=GatewayClient(
+    		address='127.0.0.1',
+    		port=25333,
+    		auto_close=True,
+    		gateway_property=None))
+
     addition_app = gateway.entry_point
     print addition_app.testAdd(2,9)
     print addition_app.addition(2,10)
